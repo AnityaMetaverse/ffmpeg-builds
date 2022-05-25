ce71a286e8af025674fe25869b28e6a35bd74744
Michael Alexsander <michaelalexsander@protonmail.com>
2022-05-25 00:37:29 -0300

    Initial commit

---
+++ build-scripts/linux.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+cd ../FFmpeg-n4.4.2
+
+./configure \
+	--prefix=../bin/linux \
+	--target-os=linux \
+	--enable-pthreads \
+	--enable-shared \
+	--enable-libvpx \
+	--enable-libwebp \
+	--enable-openssl \
+	--enable-zlib \
+	--enable-pic \
+	--disable-static \
+	--disable-debug \
+	--disable-sdl2 \
+	--disable-libx264 \
+	--disable-libx265 \
+	--disable-programs \
+	--disable-doc \
+	--disable-manpages \
+	--disable-podpages \
+	--disable-txtpages \
+	--disable-ffplay \
+	--disable-ffprobe \
+	--disable-ffmpeg
+
+make clean
+make install
